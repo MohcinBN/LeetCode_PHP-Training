992a04e889b8034922e6a6d1fb995f3661ab4ec4
Mohcin Bounouara <rapmabiz@gmail.com>
2022-12-28 14:37:33 +0100

    Update Numbers_Disappeared_in_an_Array.php
    second change

--- Numbers_Disappeared_in_an_Array.php
+++ Numbers_Disappeared_in_an_Array.php
@@ -1,15 +1,9 @@
     function findDisappearedNumbers($nums) {
         
-        $disappeared = [];
-        $count_of_nums = count($nums);
-        
-        for($i=1; $i < $count_of_nums + 1; $i++) {
-            
-            if(!in_array($i, $nums)) {
-                $disappeared[] = $i;
-            }
-        }
-        
-        return $disappeared;
+       $arry_count = count($nums);
+
+       $range = range(1, $arry_count);
+
+       return array_diff($range , $nums);
 
     }
